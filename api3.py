5e8a50d0d4e47ea8955ca6d178d084a70ea07a8a
yennanliue <f339339@gmail.com>
2017-06-01 01:18:19 +0800

    finish  api db data delete command

--- api3.py
+++ api3.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, request
+from flask import Flask, request,jsonify,abort,request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+from sqlalchemy.orm import sessionmaker
+import jsonify
 
 
 
-# ref https://impythonist.wordpress.com/2015/07/12/build-an-api-under-30-lines-of-code-with-python-and-flask/
+
+
+# https://impythonist.wordpress.com/2015/07/12/build-an-api-under-30-lines-of-code-with-python-and-flask/
 
 e = create_engine('sqlite:///movie_metadata.db')
 
+# http://docs.sqlalchemy.org/en/latest/orm/session_basics.html
+Session = sessionmaker(bind=e)
+session = Session()
+
 app = Flask(__name__)
 api = Api(app)
+
+
 
 # Get 
 #=======================================
@@ -48,28 +58,33 @@ class Sample_movie_meta(Resource):
 # Delete
 #=======================================
 
+
 # http://blog.cloudoki.com/getting-started-with-restful-apis-using-the-flask-microframework-for-python/
 # http://www.bradcypert.com/writing-a-restful-api-in-flask-sqlalchemy/
 
 
-#class delete_movie_data(Resource):
-#    def delete(self, movie_name):
-#        conn = e.connect()
-#        query = conn.execute("delete from movie where movie_title = '%s'"%movie_name)
-#        conn.commit()
-#        result = conn.execute("select count(*) from movie")
-#        return result 
+
+@app.route('/del/<string:country_name>', methods=['GET','POST','DELETE'])
+### NEED TO INCLUDE GET, POST method as well when uding DELETE or other  REST command
+### https://www.youtube.com/watch?v=q5rIxpE3fjA
+def deleteProduct(country_name):
+    print ('start')
+    conn = e.connect()
+    conn.execute("delete from movie where country ='%s' "%country_name)
+    session.commit()
+    print ('Delete success!')
+    return "OK delete country data done"
 
 
-#@app.route('/delete_movie/<string:movie_name>', methods = ['DELETE'])
-#def delete(self, movie_name):
+#class delete_country_data(Resource):
+#    def delete(self, country_name):
+#        print ('start')
 #        conn = e.connect()
-#        #del_st = user_t.delete().where(user_t.c.l_name == 'Hello')    
-#        conn.delete(movie_title == 'Tangled')  
-#        conn.execute() 
-#
-#        result = conn.execute("select count(*) from movie")
-#        return result 
+#        conn.execute("delete from movie where country = '%s'"%country_name)
+#        session.commit()
+#       #result = conn.execute("select count(*) from movie")
+#        return jsonify( { 'result': True } )
+ 
 
 #=======================================
 
@@ -80,7 +95,7 @@ api.add_resource(Director_Meta, '/Director_Meta')
 api.add_resource(Country_imdb_meta, '/countryname/<string:country_name>')
 api.add_resource(Sample_movie_meta, '/moviedata/<string:limit_count>')
 
-#api.add_resource(delete_movie_data, '/delete_movie/<string:movie_name>')
+#api.add_resource(delete_country_data, '/delete_country/<string:country_name>')
 
 
 
